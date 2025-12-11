21c265b135b41b9b38f623953121fcd4be1103ad
Lowell-god <asahanlowellaldrich@gmail.com>
2025-12-11 22:52:37 +0900

    feat: fix linkedin address

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -15,14 +15,14 @@ function Main() {
         <div className="content">
           <div className="social_icons">
             <a href="https://github.com/Lowell-god" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://www.linkedin.com/in/lowell-aldrich-asahan-136275392" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
           <h1>Lowell Aldrich</h1>
           <p>Full Stack AI Engineer</p>
 
           <div className="mobile_social_icons">
             <a href="https://github.com/Lowell-god" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://www.linkedin.com/in/lowell-aldrich-asahan-136275392" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
         </div>
       </div>
